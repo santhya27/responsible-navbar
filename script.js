632dc51e5ef5f834013d2e2071f71c5342ce0065
santhya27 <santhya0906@gmail.com>
2025-07-06 14:05:53 +0530

    Initial upload of responsive navbar project
    Uploaded index.html, style.css, script.js and logo image.

---
+++ script.js
@@ -0,0 +1,4 @@
+// Toggle the menu on small screens
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  document.getElementById("nav-links").classList.toggle("show");
+});
